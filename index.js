0c0fe3a642fc0c6952ece1b103986840b3c25b10
hoangnam34243 <arechiga06nam1311@outlook.com>
2024-05-13 23:56:51 +0700

    update

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
-function sortedArrayToBST(nums) {
-  if (nums.length === 0) return null;
-  const mid = Math.floor(nums.length / 2);
-  const root = new TreeNode(nums[mid]);
-  root.left = sortedArrayToBST(nums.slice(0, mid));
-  root.right = sortedArrayToBST(nums.slice(mid + 1));
-  return root;
+function capitalizeWords(str) {
+  return str
+    .split(" ")
+    .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+    .join(" ");
 }
